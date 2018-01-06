9e6fbbc7aa22a17bfb1b21b34f895e428a389785
l3lackclevil <l3lackclevil@hotmail.com>
2018-01-06 15:50:28 +0700

    beautiful

--- components/IndexNavigator.js
+++ components/IndexNavigator.js
@@ -10,13 +10,29 @@ const Container = styled.div`
 
     & > .container > div {
         display: inline-block;
+        
     }
+    & > .container > div:hover {
+        display: inline-block;
+        pointer:cursor;
+        
+    }
+
 
     & > .container > div:nth-child(n+2) {
         float: right;
         margin-right : 50px; 
         margin-top : 15px;
+        
+        
     }
+    & > .container > div:nth-child(n+2){
+        pointer: cursor;
+    }
+    & > .container > div:nth-child(2){
+        font-size:20px;
+    }
+
 `
 
 const Navigator = () => (
@@ -27,8 +43,8 @@ const Navigator = () => (
             </div>
             <div>
                 <i class="zmdi zmdi-search"></i>&nbsp;&nbsp;
-                <i class="zmdi zmdi-account-o"></i>
-
+                <i class="zmdi zmdi-account-o"></i>&nbsp;&nbsp;
+                <i class="zmdi zmdi-shopping-cart"></i>
             </div>
             <div>CONTACT</div>
             <div>HOW TO ORDER</div>            
--- containers/header.js
+++ containers/header.js
@@ -2,10 +2,6 @@ import { Component } from 'react'
 
 import Banner from '../components/Banner'
 import Navigator from '../components/Navigator'
-<<<<<<< HEAD
-
-=======
->>>>>>> af7956ebb5a6b0595d11f3177b6ff4019e030840
 
 export default class extends Component {
     render() {
