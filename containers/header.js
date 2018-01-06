06da3a41280707abc8622b5832bf76f38c3b5d28
chunza2542 <chunza2542@hotmail.com>
2018-01-06 15:06:56 +0700

    fix bugs

--- containers/header.js
+++ containers/header.js
@@ -2,7 +2,6 @@ import { Component } from 'react'
 
 import Banner from '../components/Banner'
 import Navigator from '../components/Navigator'
-import CategoryContainer from '../containers/Category'
 
 export default class extends Component {
     render() {
